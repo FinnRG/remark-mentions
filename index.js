ed42fbf5c6719968fc1934a631195f4eeea64ff6
FinnRG <finngaertner@protonmail.com>
2023-08-01 17:10:19 +0200

    chores: Add tests

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 /**
  * @typedef {import('./lib/index.js').Options} Options
- * @typedef {import('./lib/index.js').BuildUrl} BuildUrl
- * @typedef {import('./lib/index.js').DefaultBuildUrl} DefaultBuildUrl
- * @typedef {import('./lib/index.js').BuildUrlValues} BuildUrlValues
- * @typedef {import('./lib/index.js').BuildUrlCommitValues} BuildUrlCommitValues
- * @typedef {import('./lib/index.js').BuildUrlCompareValues} BuildUrlCompareValues
- * @typedef {import('./lib/index.js').BuildUrlIssueValues} BuildUrlIssueValues
- * @typedef {import('./lib/index.js').BuildUrlMentionValues} BuildUrlMentionValues
  */
 
 export { default } from "./lib/index.js";
---
+++ test/index.js
@@ -0,0 +1,37 @@
+/**
+ * @typedef {import('../index.js').Options} Options
+ */
+import test from "tape";
+import remarkMentions from "../index.js";
+import { remark } from "remark";
+
+import { VFile } from "vfile";
+
+test("remark-mentions", (t) => {
+  t.equal(typeof remarkMentions, "function", "should be a function");
+
+  t.doesNotThrow(() => {
+    remark().use(remarkMentions).freeze();
+  }, "should not throw if not passed options");
+
+  t.equal(process("@test"), "[**@test**](/test)\n");
+
+  t.equal(process("This is @test"), "This is [**@test**](/test)\n");
+
+  t.equal(
+    process("@test", { usernameLink: (username) => `/Profile/${username}` }),
+    "[**@test**](/Profile/test)\n"
+  );
+
+  t.end();
+});
+
+/***
+ * Shortcut to process.
+ *
+ * @param {string|VFile} value
+ * @param {Options} [options]
+ */
+function process(value, options) {
+  return remark().use(remarkMentions, options).processSync(value).toString();
+}
